6585b5c1e593149d13192a8246f3e87a2ebd5a74
Alex Oliveira <alexoliveira7x@gmail.com>
2024-08-13 05:35:40 +0100

    avoid barrel and remove rule

--- packages/toolkit/src/index.ts
+++ packages/toolkit/src/index.ts
@@ -1,14 +1,17 @@
 export {
   default as removeNilKeys,
-  removeNilKeysImultable
-} from '@guildadev/remove-nil-keys';
+  removeNilKeysImultable,
+} from "@guildadev/remove-nil-keys";
 
 export {
   default as removeFalsyKeys,
-  removeFalsyKeysImultable
-} from '@guildadev/remove-falsy-keys';
+  removeFalsyKeysImultable,
+} from "@guildadev/remove-falsy-keys";
 
-export type { removeNilKeysProps } from '@guildadev/remove-nil-keys';
-export type { RemoveFalsyKeysProps } from '@guildadev/remove-falsy-keys';
+export type { removeNilKeysProps } from "@guildadev/remove-nil-keys";
+export type { RemoveFalsyKeysProps } from "@guildadev/remove-falsy-keys";
 
-export * from '@guildadev/get-initials';
+export {
+  default as getInitials,
+  type getInitialsParam,
+} from "@guildadev/get-initials";
