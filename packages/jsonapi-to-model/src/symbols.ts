57fa78193b5b0f116b9c72df15ff30bcd2cac125
Alex Oliveira <alexoliveira7x@gmail.com>
2024-08-14 10:00:26 +0100

    feat: add support for custom and links associations

--- packages/jsonapi-to-model/src/symbols.ts
+++ packages/jsonapi-to-model/src/symbols.ts
@@ -2,3 +2,5 @@ export const kClassMapping = Symbol.for("class_mapping");
 export const METAS_METADATA_KEY = Symbol.for("metas");
 export const ATTRIBUTE_METADATA_KEY = Symbol.for("attributes");
 export const INCLUDED_METADATA_KEY = Symbol.for("includeds");
+export const LINKS_METADATA_KEY = Symbol.for("Links");
+export const INCLUDED_LINKS_METADATA_KEY = Symbol.for("IncludedLinks");
